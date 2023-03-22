b5c15f6354fa5e1c623ef601dbc2f748aaed7155
“Huriye_Dgit config --global user.email “demirogluhu@gmail.com <demirogluhu@gmail.com>
2023-03-22 11:32:06 -0500

    deployed

--- src/components/form/FormComponent.js
+++ src/components/form/FormComponent.js
@@ -31,19 +31,7 @@ const FormComponent = ({info, setInfo, handleFormSubmit}) => {
       direction="column"
       style={{ width: "300" }}
     >
-      <p className="contact-header">
-        <div>
-          <a
-            href="https://github.com/clarusway"
-            className="design"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <code>{"<Clarusway/> "}</code>
-          </a>
-        </div>
-        <span className="design header">design</span>
-      </p>
+      
       <h2 className="contact-header">Add Contact</h2>
       <Box style={{ backgroundColor: "white", padding: "20px" }}>
         <form onSubmit={handleFormSubmit}>
